a71822ed3fb4f85c70bc6711183aec528c1dbe81
Alexander Lindermayr <linderal@uni-bremen.de>
2020-05-10 13:32:01 +0200

    Plots

--- scripts/plot.py
+++ scripts/plot.py
@@ -6,17 +6,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-PLOT_MEAN = 0
-PLOT_MAX = 1
-
-
 def create_arg_parser():
     parser = argparse.ArgumentParser(
         description='Plot results from kserver simulation')
     parser.add_argument('sampleFile')
     parser.add_argument('-b', '--bin_size', default=0.25)
-    parser.add_argument('--max', action='store_const',
-                        const=1, default=0, dest='mode')
+    parser.add_argument('--max', action='store_true')
+
     return parser
 
 
@@ -36,20 +32,26 @@ def robust(x):
     return 1 + 1/x
 
 
-def plot_lambda(df, eta_res, mode):
+def plot_lambda(df, eta_res, args):
     df['Bin'] = np.ceil(df['EtaOverOpt'] / eta_res) * eta_res
     # df = df[df['Bin'] < 10]
     dfAlg = df.loc[:, ['Lmbda', 'CRalg', 'Bin']]
     dfDC = df.loc[:, ['Lmbda', 'CRdc']]
-    if mode == PLOT_MAX:
-        ax = dfAlg.groupby(
-            ['Bin', 'Lmbda']).max().unstack('Bin').plot(style='--', legend=True)
-        dfDC.groupby(['Lmbda']).max().plot(ax=ax)
 
-    if mode == PLOT_MEAN:
-        ax = dfAlg.groupby(
-            ['Bin', 'Lmbda']).mean().unstack('Bin').plot(style='--', legend=True)
-        dfDC.groupby(['Lmbda']).mean().plot(ax=ax)
+    if args.max:
+        grouped_data = dfAlg.groupby(
+            ['Bin', 'Lmbda']).max().unstack('Bin')
+        ax = dfDC.groupby(['Lmbda']).max().plot(
+            label='DoubleCoverage', legend=True)
+    else:
+        grouped_data = dfAlg.groupby(
+            ['Bin', 'Lmbda']).mean().unstack('Bin')
+        ax = dfDC.groupby(['Lmbda']).mean().plot(
+            label='DoubleCoverage', legend=True)
+
+    for label, l in list(grouped_data):
+        grouped_data[(label, l)].plot(ax=ax,
+                                      style='--', label=f"Eta/Opt<={l:1.2f}", legend=True)
 
     plt.plot((0, 1), (1, 1), label='OPT')
 
@@ -68,15 +70,25 @@ def plot_lambda(df, eta_res, mode):
     fig.subplots_adjust(right=0.7)
 
 
-def plot_eta(df, eta_res):
+def plot_eta(df, eta_res, args):
     df['Bin'] = np.ceil(df['EtaOverOpt'] / eta_res) * eta_res
     max_bin = df['Bin'].max()
     dfAlg = df.loc[:, ['Lmbda', 'CRalg', 'Bin']]
     dfDC = df.loc[:, ['CRdc', 'Bin']]
-    ax = dfAlg.groupby(
-        ['Bin', 'Lmbda']).mean().unstack('Lmbda').plot(style='--', label='test', legend=True)
 
-    dfDC.groupby(['Bin']).mean().plot(ax=ax)
+    if args.max:
+        grouped_data = dfAlg.groupby(['Bin', 'Lmbda']).max().unstack('Lmbda')
+        ax = dfDC.groupby(['Bin']).max().plot(
+            label='DoubleCoverage', legend=True)
+    else:
+        grouped_data = dfAlg.groupby(['Bin', 'Lmbda']).mean().unstack('Lmbda')
+        ax = dfDC.groupby(['Bin']).mean().plot(
+            label='DoubleCoverage', legend=True)
+
+    for label, l in list(grouped_data):
+        grouped_data[(label, l)].plot(ax=ax,
+                                      style='--', label=f"Lambda={l:1.1f}", legend=True)
+
     plt.plot((0, max_bin), (1, 1), label='OPT')
     plt.xlabel('Eta / Opt')
     plt.ylabel('Competitive ratio')
@@ -94,8 +106,8 @@ if __name__ == "__main__":
     if os.path.exists(parsed_args.sampleFile):
         print(parsed_args.bin_size)
         data = get_data(parsed_args.sampleFile)
-        plot_eta(data, float(parsed_args.bin_size))
-        plot_lambda(data, float(parsed_args.bin_size), parsed_args.mode)
+        plot_eta(data, float(parsed_args.bin_size), parsed_args)
+        plot_lambda(data, float(parsed_args.bin_size), parsed_args)
         plt.show()
     else:
         print("Path not valid!")
